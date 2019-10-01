95a5cabca0b01c1b90b04217eda25f105ad5b74f
Nguyen Tran <kevtran07@gmail.com>
2019-10-01 22:54:26 +0300

    UI - login view

---
+++ app/src/main/java/fi/metropolia/deliverytracker/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package fi.metropolia.deliverytracker.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
